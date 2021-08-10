934f5a23a2e93043d0fc34e1f64221bfe9f51998
SILBIA <SILBIA@SILBIA>
2021-08-10 19:21:41 +0900

    21.08.10

---
+++ src/Ex4_test4_5.java
@@ -0,0 +1,17 @@
+
+public class Ex4_test4_5 {
+
+	public static void main(String[] args) {
+		int i = 0;
+		while(i<=0) {
+			int j = 0;
+			while(j<=i) {
+				System.out.println("*");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+
+}
